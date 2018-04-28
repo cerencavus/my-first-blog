bae22dc59d6fd23dd5461bff134f54d00734df75
cerencavus <cerencavus@hotmail.com>
2018-04-28 18:41:25 +0300

    My Django Girls app, second commit

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Post
 def post_list(request):
-	return render(request, 'blog/post_list.html', {})
+	posts=Post.objects.all().order_by('created_date') 
+	return render(request, 'blog/post_list.html', {'posts':posts})
 # Create your views here.
